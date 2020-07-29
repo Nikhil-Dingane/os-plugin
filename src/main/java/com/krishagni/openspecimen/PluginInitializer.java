c96c0a70e7b1e16e4fc67fbc8bc7c386405de094
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2020-07-29 12:04:33 +0530

    SI: NPE fixed and some refactoring changes

--- src/main/java/com/krishagni/openspecimen/PluginInitializer.java
+++ src/main/java/com/krishagni/openspecimen/PluginInitializer.java
@@ -5,16 +5,15 @@ import org.springframework.beans.factory.InitializingBean;
 import com.krishagni.catissueplus.core.common.domain.LabelTmplTokenRegistrar;
 
 public class PluginInitializer implements InitializingBean {
-     
-    private LabelTmplTokenRegistrar labelTokensRegistrar;
-     
-    public void setLabelTokensRegistrar(LabelTmplTokenRegistrar labelTokensRegistrar) {
-        this.labelTokensRegistrar = labelTokensRegistrar;
-    }
- 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        labelTokensRegistrar.register(new RegistrationEnglishLetterSequence()); 
- 
-    }
+
+	private LabelTmplTokenRegistrar labelTokensRegistrar;
+
+	public void setLabelTokensRegistrar(LabelTmplTokenRegistrar labelTokensRegistrar) {
+		this.labelTokensRegistrar = labelTokensRegistrar;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		labelTokensRegistrar.register(new RegistrationEnglishLetterSequence());
+	}
 }
--- src/main/java/com/krishagni/openspecimen/RegistrationEnglishLetterSequence.java
+++ src/main/java/com/krishagni/openspecimen/RegistrationEnglishLetterSequence.java
@@ -1,16 +1,18 @@
 package com.krishagni.openspecimen;
 
-import com.krishagni.catissueplus.core.biospecimen.domain.Specimen;
-import com.krishagni.catissueplus.core.common.domain.AbstractGreekLetterSequenceToken;
+import org.springframework.beans.factory.annotation.Configurable;
 
-public class RegistrationEnglishLetterSequence extends AbstractEnglishLetterSequenceToken<Specimen>{
-	
+import com.krishagni.catissueplus.core.biospecimen.domain.Specimen;
+
+@Configurable
+public class RegistrationEnglishLetterSequence extends AbstractEnglishLetterSequenceToken<Specimen> {
+
 	public RegistrationEnglishLetterSequence() {
 		this.name = "REG_ENG_SEQ";
 	}
+
 	@Override
 	protected Integer getSequence(Specimen spmn) {
 		return getUniqueId(spmn.getRegistration().getId().toString());
 	}
-
 }
